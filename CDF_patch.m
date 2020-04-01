c2464deb8cf49d0a1672fd56ad0d722a2a461d8c
Duo Chan <duochan@Duos-MacBook-Pro.local>
2020-04-01 06:55:08 -0400

    Add transparency to CDF_patch

--- CDF_patch.m
+++ CDF_patch.m
@@ -1,8 +1,18 @@
-% CDF_patch(x,y,col,a)
-function h = CDF_patch(x,y,col,a)
+% CDF_patch(x,y,col,a,P)
+function h = CDF_patch(x,y,col,a,P)
 
     if nargin<4
         a = 0.05;
+    end
+
+    if nargin<5
+        alp = 0.5;
+    else
+        if isfield(P,'alpha')
+          alp = P.alpha;
+        else
+          alp = 0.5;
+        end
     end
     
     if 1,
@@ -15,14 +25,14 @@ function h = CDF_patch(x,y,col,a)
         up = nanmean(y,1) + s * abs(norminv(a/2));
         low = nanmean(y,1) - s * abs(norminv(a/2));
     end
-    
+
     logic = ~isnan(up);
     x = x(logic);
     up = up(logic);
     low = low(logic);
-    
+
     h = patch([x fliplr(x)],[up fliplr(low)],col);
-    alpha(h,0.5)
+    alpha(h,alp)
     set(h,'linest','none')
 
 end
